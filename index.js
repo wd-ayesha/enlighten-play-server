9a24de172948d76ea6f3ee0e6efe9e29bcffe1e4
Ayesha <ayesha18mbstu@gmail.com>
2023-05-21 23:41:26 +0600

    allToys fetch from db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,7 +11,6 @@ app.use(express.json());
 
 console.log(process.env.DB_PASS);
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ss27qd4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,16 +19,26 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
+
+    const toysCollection = client.db("enlightenToys").collection("allToys");
+
+    app.get("/allToys", async (req, res) => {
+      const result = await toysCollection.find({}).toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -37,11 +46,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('enlighten is running')
-})
+app.get("/", (req, res) => {
+  res.send("enlighten is running");
+});
 
 app.listen(port, () => {
-    console.log(`Enlighten Play is running on port ${port}`)
-})
+  console.log(`Enlighten Play is running on port ${port}`);
+});
